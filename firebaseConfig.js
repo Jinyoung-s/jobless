a901e69f52a28dd1554b954d525efb0d1567937b
Jinyoung So <sojinyoung23@gmail.com>
2023-02-03 17:36:37 -0500

    firebase setting

---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOYMN0CiXA6kKcqhrt0lo394zav_U8aEM',
+  authDomain: 'com.unemployed.jobless',
+  databaseURL: 'https://jobless-1ad8f.firebaseio.com',
+  projectId: 'jobless-1ad8f', 
+  storageBucket: 'jobless-1ad8f.appspot.com',
+  messagingSenderId: '938814888188',
+  appId: '1:938814888188:android:e81823d91f203e6d671f79',
+};
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
