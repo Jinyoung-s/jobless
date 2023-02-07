eab3fe044d6f0993bb765d4d4bd9d104bdfa98d1
Jinyoung So <sojinyoung23@gmail.com>
2023-02-06 23:50:11 -0500

    adding RegisterPage by Francis

--- App.js
+++ App.js
@@ -1,33 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginPage from './src/screens/LoginPage'
-import Sample from './src/screens/Sample'
-import HomePage from './src/screens/HomePage'
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginPage from "./src/screens/LoginPage";
+import Sample from "./src/screens/Sample";
+import HomePage from "./src/screens/HomePage";
+import RegisterPage from "./src/screens/RegisterPage";
 
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown : false}} name="Sample" component={Sample} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="LoginPage"
+          component={LoginPage}
+        />
         <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="RegisterPage" component={RegisterPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
