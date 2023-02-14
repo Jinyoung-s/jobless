7b18117d193c8fcc9204f8f2b3bf7e924b63c1a1
Jinyoung So <sojinyoung23@gmail.com>
2023-02-13 20:09:20 -0500

    installed tab navigation

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
-import { auth } from '../../firebaseConfig';
+import React, { Component } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { auth, db, collection, addDoc } from "../../firebaseConfig";
 
-
-export default class HomePage extends Component {  
+export default class HomePage extends Component {
   render() {
     const handleLogout = () => {
-        auth.signOut()
-            .then((userCredential) => {       
-            console.log('LogOut successful');
-            })
-            .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            });
-    }; 
-     
+      auth
+        .signOut()
+        .then((userCredential) => {
+          console.log("LogOut successful");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    };
+
+    const handleDatabase = async () => {
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+          first: "Ada",
+          last: "Lovelace",
+          born: 1815,
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    };
+
     return (
       <View>
         <Text> Home Screen </Text>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Sign Out</Text>
-      </TouchableOpacity>
-      </View> 
-    )
+        <TouchableOpacity onPress={handleLogout}>
+          <Text>Sign Out</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleDatabase}>
+          <Text>insert</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
